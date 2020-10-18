826e2d48f0a0ee21b161971495a675365d3e3880
VSRUJANA <srujana.valavala@gmail.com>
2020-10-18 12:04:18 +0530

    [srujana] Add. Contact class and AddressBook class are added

---
+++ Address_Book_Using_Collections/AddressBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book_Using_Collections
+{
+	public class AddressBook
+	{
+		public static List<Contact> contactList;
+		public AddressBook()
+		{
+			contactList = new List<Contact>();
+		}
+	}
+}
---
+++ Address_Book_Using_Collections/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Address_Book_Using_Collections
+{
+	public class Contact
+	{
+		// Properties Declaration
+		public string firstName { get; set; }
+		public string lastName { get; set; }
+		public string address { get; set; }
+		public string city { get; set; }
+		public string state { get; set; }
+		public string zipCode { get; set; }
+		public string phoneNumber { get; set; }
+		public string email { get; set; }
+
+		// Constructor Declaration
+		public Contact(string fName, string lName, string address, string city, string state, string zip, string phNo, string eId)
+		{
+			this.firstName = fName;
+			this.lastName = lName;
+			this.address = address;
+			this.city = city;
+			this.state = state;
+			this.zipCode = zip;
+			this.phoneNumber = phNo;
+			this.email = eId;
+		}
+	}
+}
